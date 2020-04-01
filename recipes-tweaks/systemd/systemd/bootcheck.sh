bd90ab8e2fafd6c9878a74ec4c586de4b478b0cf
Peter Fink <pfink@christ-es.de>
2020-04-01 15:31:58 +0200

    systemd-bootcheck: auto determine the correct device
    When the correct device is determined also update /etc/fw_env.config
    
    Signed-off-by: Peter Fink <pfink@christ-es.de>

--- recipes-tweaks/systemd/systemd/bootcheck.sh
+++ recipes-tweaks/systemd/systemd/bootcheck.sh
@@ -1,5 +1,31 @@
 #!/bin/sh
 
+if [ -f "/sys/block/mmcblk3boot0/force_ro" ] ; then
+    # kernel 4.1 maps USDHC4 to mmcblk3
+    : ${MMCDEV:=3}
+else
+    # kernel 4.19 maps USDHC4 to mmcblk1 if some other USDHC device is configured
+    # and mmcblk0 if this is the only one
+    # so use the number of usdhc interfaces to determine the correct mmc device
+    MMCCOUNT=$(for i in /proc/device-tree/soc/aips-bus@*/usdhc\@* ; do cat $i/status; echo ""; done | grep -a okay | wc -l)
+    if [ "$MMCCOUNT" -gt 1 ] ; then
+        : ${MMCDEV:=1}
+    else
+        : ${MMCDEV:=0}
+    fi
+fi
+
+# try to update /etc/fw_env.config with the device number we found out above
+mv /etc/fw_env.config /etc/fw_env.config.bak 2>/dev/null
+head -n -1 /etc/fw_env.config.bak > /etc/fw_env.config 2>/dev/null
+echo "/dev/mmcblk${MMCDEV}boot0      0x300000        0x2000" >> /etc/fw_env.config 2>/dev/null
+if [ -e /etc/fw_env.config ]; then
+	rm /etc/fw_env.config.bak 2>/dev/null
+fi
+
+# if we failed here somehow (e.g. if /etc/ is mounted RO) try to continue anyway
+
+
 for boot0dev in /sys/block/mmcblk*boot0; do
     echo 0 > $boot0dev/force_ro
 done
